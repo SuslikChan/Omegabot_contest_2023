08e2d9ab67f491d5dc5fbcf911c01728ae1175ae
SuslikChan <bobamar.maryin@gmail.com>
2023-10-16 10:54:18 +0300

    len_sensor

---
+++ fragments/len_sensor/len_sensor.ino
@@ -0,0 +1,26 @@
+#define SENS_PIN A0 
+
+
+float len_sensor(){ // Чтение дистанции с ИК дальномера
+
+  float measur  = analogRead(SENS_PIN);     // Чтение значения с АЦП
+  float voltage = measur * 5 / 1023;        // Преобразование АЦП в реальный вольтаж
+  float dist    = 65 * pow(voltage, -1.10); // Преобразование волтажа в мм
+
+  return dist
+
+  }
+
+
+void setup() {
+  
+  pinMode(SENS_PIN, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop(){
+
+  Serial.println(len_sensor());
+  delay(500);
+}
