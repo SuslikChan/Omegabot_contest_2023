580ba477c83d9a90de3071d69e11c752117d3f0b
kibberpank <dowakin1707@mail.ru>
2023-10-24 19:15:45 +0300

    Update turn.cpp

--- turn_90_degrees/turn.cpp
+++ turn_90_degrees/turn.cpp
@@ -1,22 +1,20 @@
 #include "turn.h"
 
-void turnRight(int angle){
+void turn(int angle){
   long int time_0 = millis();
   float degrees = 0.0;
+  if (angle > 0){
   while (degrees < angle){
     degrees, time_0 = get_degrees(degrees, time_0);
     SetTankSpeed(150, -150);  
   }
-  SetTankSpeed(0,0);
-}
-
-
-void turnLeft(int angle){
-  long int time_0 = millis();
-  float degrees = 0.0;
-  while (degrees > -angle){
+  }
+  else{
+    while (degrees > angle){
     degrees, time_0 = get_degrees(degrees, time_0);
     SetTankSpeed(-150, 150);  
   }
+  }
   SetTankSpeed(0,0);
 }
+
